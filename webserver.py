2305bf2a124a844bc4061e547bc3eb56983c1788
NighT-Fa1l <safawatsayam@gmail.com>
2024-10-21 06:47:43 +0600

    Update webserver.py

--- webserver.py
+++ webserver.py
@@ -1,14 +1,25 @@
 from flask import Flask
-from threading import Thread
+import os
+import threading
 
-app = Flask('')
+app = Flask(__name__)
+
 @app.route('/')
 def home():
-  return "discord bot is ok"
+    return "Bot is running!"
 
 def run():
-  app.run(host="0.0.0.0", port=8080)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
 
-def keep_alive():
-  t = Thread(target=run)
-  t.start()
+if __name__ == "__main__":
+    threading.Thread(target=run).start()
+    
+    # Your Discord bot code below
+    import discord
+    client = discord.Client()
+
+    @client.event
+    async def on_ready():
+        print(f'Logged in as {client.user}')
+
+    client.run(os.getenv('DISCORD_TOKEN'))
